3055ed7d69cde433aeace31d5a42130b7716f42f
KnightZJ <223754547@qq.com>
2022-07-19 22:07:04 +0800

    📦 NEW: complete basic gameflow

--- include/tixtax.h
+++ include/tixtax.h
@@ -1,3 +1,11 @@
+#include <string>
+using namespace std;
+
+enum CellColor {
+    None,
+    Red,
+    Blue
+};
 
 enum RoomStatus {
     Available,
@@ -5,19 +13,24 @@ enum RoomStatus {
     RedRoom,
     BlueRoom
 };
+
+enum GameStatus {
+    Gaming,
+    RedWin,
+    BlueWin,
+    Tie
+};
+
 class TixTaxGame {
 public:
     class Room {
     public:
-        enum CellColor {
-            None,
-            Red,
-            Blue
-        };
-    public:
         Room();
-        bool setColor(int row, int col, CellColor color);
+        bool move(int row, int col, CellColor color);
+        bool canMove(int row, int col, CellColor color);
         RoomStatus getStatus();
+        void updateStatus();
+        string print(int row);
     private:
         int red, blue;
         RoomStatus status;
@@ -25,8 +38,16 @@ public:
 public:
     TixTaxGame();
     void start();
+    bool move(int cellRow, int CellCol, CellColor color);
+    bool canMove(int cellRow, int CellCol, CellColor color);
+    GameStatus getStatus();
+    void updateStatus();
+    string print();
 private:
     Room rooms[3][3];
-    int redroom, blueroom;
+    int redroom, blueroom, fullroom;
     int round;
+    int lastRoomRow, lastRoomCol;
+    bool global;
+    GameStatus status;
 };
--- src/tixtax.cpp
+++ src/tixtax.cpp
@@ -5,7 +5,10 @@ TixTaxGame::TixTaxGame() {
 }
 
 void TixTaxGame::start() {
-    redroom = blueroom = 0;
+    redroom = blueroom = fullroom = 0;
+    round = 1;
+    status = Gaming;
+    global = true;
     for (int i = 0; i < 3; ++i)
         for (int j = 0; j < 3; ++j)
             rooms[i][j] = Room();
@@ -20,29 +23,130 @@ RoomStatus check(int red, int blue) {
     if ((red & 0x7) == 0x7 || (red & 0x38) == 0x38 || (red & 0x1C0) == 0x1C0
       || (red & 0x49) == 0x49 || (red & 0x92) == 0x92 || (red & 0x124) == 0x124
       || (red & 0x111) == 0x111 || (red & 0x54) == 0x54)
-        return RoomStatus::RedRoom;
+        return RedRoom;
     else if ((blue & 0x7) == 0x7 || (blue & 0x38) == 0x38 || (blue & 0x1C0) == 0x1C0
       || (blue & 0x49) == 0x49 || (blue & 0x92) == 0x92 || (blue & 0x124) == 0x124
       || (blue & 0x111) == 0x111 || (blue & 0x54) == 0x54)
-        return RoomStatus::BlueRoom;
+        return BlueRoom;
     else if ((blue | red) == 0x1ff)
-        return RoomStatus::Full;
-    return RoomStatus::Available;
+        return Full;
+    return Available;
 }
 
-bool TixTaxGame::Room::setColor(int row, int col, CellColor color) {
+bool TixTaxGame::Room::canMove(int row, int col, CellColor color) {
     if (status != Available)
         return false;
-    int hand = 1 << (row*3+col);
-    if (red & hand || blue & hand || color == None)
-        return false;
-    if (color == Red)
-        red |= hand;
-    else
-        blue |= hand;
-    return true;
+    int mask = 1 << (row*3+col);
+    return !(red & mask || blue & mask || color == None);
+}
+
+bool TixTaxGame::Room::move(int row, int col, CellColor color) {
+    if (canMove(row, col, color)) {
+        int mask = 1 << (row*3+col);
+        if (color == Red)
+            red |= mask;
+        else
+            blue |= mask;
+        updateStatus();
+        return true;
+    }
+    return false;
+}
+
+void TixTaxGame::Room::updateStatus() {
+    status = check(red, blue);
 }
 
 RoomStatus TixTaxGame::Room::getStatus() {
-    return check(red, blue);
+    return status;
+}
+
+string TixTaxGame::Room::print(int row) {
+    if (status == RedRoom)
+        switch (row) {
+            case 0: return "   A   ";
+            case 1: return "  AAA  ";
+            case 2: return " AAAAA ";
+        }
+    else if (status == BlueRoom)
+        switch (row) {
+            case 0: return " BBBBB ";
+            case 1: return " BBB   ";
+            case 2: return " BBBBB ";
+        }
+    else if (status == Full)
+        return " # # # ";
+    int mask = 0x1 << (row * 3);
+    string s = " ";
+    for (int i = 0; i < 3; ++i) {
+        if ((red & mask) == mask)
+            s += "A ";
+        else if ((blue & mask) == mask)
+            s += "B ";
+        else
+            s += "  ";
+        mask <<= 1;
+    }
+    return s;
+}
+
+string TixTaxGame::print() {
+    string s = "  -------    -------    -------\n";
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            for (int k = 0; k < 3; ++k)
+                s += " |" + rooms[i][k].print(j) + "| ";
+            s += "\n";
+        }
+        s += "  -------    -------    -------\n";
+    }
+    return s;
+}
+
+bool TixTaxGame::canMove(int cellRow, int cellCol, CellColor color) {
+    return status == Gaming && (round % 2 == 1 ? color == Red : color == Blue) 
+            && (global || lastRoomRow == cellRow/3 && lastRoomCol == cellCol/3) 
+            && rooms[cellRow/3][cellCol/3].canMove(cellRow%3, cellCol%3, color);
+}
+
+bool TixTaxGame::move(int cellRow, int cellCol, CellColor color) {
+    if (canMove(cellRow, cellCol, color)) {
+        lastRoomRow = cellRow % 3;
+        lastRoomCol = cellCol % 3;
+        rooms[cellRow/3][cellCol/3].move(cellRow%3, cellCol%3, color);
+        switch (rooms[cellRow/3][cellCol/3].getStatus()) {
+            case RedRoom:
+                redroom |= (1 << ((cellRow/3)*3 + cellCol/3));
+                break;
+            case BlueRoom:
+                blueroom |= (1 << ((cellRow/3)*3 + cellCol/3));
+                break;
+            case Full:
+                fullroom |= (1 << ((cellRow/3)*3 + cellCol/3));
+                break;
+        }
+        updateStatus();
+        global = (rooms[lastRoomRow][lastRoomCol].getStatus() != Available);
+        ++round;
+        return true;
+    }
+    return false;
+}
+
+void TixTaxGame::updateStatus() {
+    RoomStatus roomStatus= check(redroom, blueroom);
+    switch (roomStatus) {
+        case RedRoom: status = RedWin; break;
+        case BlueRoom: status = BlueWin; break;
+        default:
+            if ((redroom | blueroom | fullroom) == 0x1ff)
+                status = Tie;
+            else
+                status = Gaming;
+            break;
+    }
+}
+
+GameStatus TixTaxGame::getStatus() {
+    return status;
 }
